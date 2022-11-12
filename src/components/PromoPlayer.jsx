e6a0a22ef49aea1bb8dd211e1877ced76228eebf
freem11 <freem1985@gmail.com>
2022-11-12 09:10:00 -0700

    fixed video resize issue for youtube videos

--- src/components/PromoPlayer.jsx
+++ src/components/PromoPlayer.jsx
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactPlayer from "react-player/youtube";
 import "./bigVids.css";
 
 function PromoPlayer() {
+
+  const [vidDimensions, setVidDimensions] = useState({
+    widthVal: window.innerWidth * 0.8,
+    heightVal: (window.innerWidth * 0.8)/ 1.77
+  })
+
+  const detectSize = () => {
+    setVidDimensions({
+      widthVal: window.innerWidth * 0.8,
+      heightVal: (window.innerWidth * 0.8)/ 1.77
+    })
+  }
+  useEffect(() => {
+    window.addEventListener('resize', detectSize)
+  
+    return() => {
+      window.removeEventListener('resize', detectSize)
+    }
+  }, [vidDimensions])
+
 
   return (
     <div className="vidbox">
       <h3 className="bigvidlabels">What is DiveGo for?</h3>
       <ReactPlayer
       url = "https://youtu.be/xwr6t4mmncc"
-      height= "350px"
-      width= "70%"
+      width={vidDimensions.widthVal}
+      height={vidDimensions.heightVal}
       controls="true"
        />
     </div>
--- src/components/WhyVidPlayer.jsx
+++ src/components/WhyVidPlayer.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactPlayer from "react-player/youtube";
 import "./bigVids.css";
 
 function WhyVidPlayer() {
 
+  const [vidDimensions, setVidDimensions] = useState({
+    widthVal: window.innerWidth * 0.8,
+    heightVal: (window.innerWidth * 0.8)/ 1.77
+  })
+
+  const detectSize = () => {
+    setVidDimensions({
+      widthVal: window.innerWidth * 0.8,
+      heightVal: (window.innerWidth * 0.8)/ 1.77
+    })
+  }
+  useEffect(() => {
+    window.addEventListener('resize', detectSize)
+  
+    return() => {
+      window.removeEventListener('resize', detectSize)
+    }
+  }, [vidDimensions])
+
   return (
     <div className="vidbox">
         <h3 className="bigvidlabels">About Us & DiveGo</h3>
+  
       <ReactPlayer
-      url = "https://youtu.be/dg-nvVdQR6g"
-      width= "70%"
+      url="https://youtu.be/dg-nvVdQR6g"
+      width={vidDimensions.widthVal}
+      height={vidDimensions.heightVal}
       controls="true"
        />
     </div>
