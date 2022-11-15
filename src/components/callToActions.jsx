69e0d5dd7bdc5327275972bc5ef112833b1fd8da
freem11 <freem1985@gmail.com>
2022-11-15 09:15:55 -0700

    recruiting drive info added

--- src/components/callToActions.jsx
+++ src/components/callToActions.jsx
@@ -3,7 +3,7 @@ import "./calltoaction.css";
 import Apple from "../images/app-store-png-logo-33116.png";
 import Google from "../images/google-play-png-logo-3799.png";
 import WebApp from "../images/WebApp2.png";
-
+import RecruitCTAButton from "./RecruitCTAButton";
 import AppleCTAButton from "./AppleCTAButton";
 import GoogleCTAButton from "./GoogleCTAButton";
 import MantaCTAButton from './MantaCTAButton';
@@ -11,9 +11,10 @@ import MantaCTAButton from './MantaCTAButton';
 function CallToActions() {
   return (
     <div className="CTAbox">
-      <AppleCTAButton />
+      <RecruitCTAButton />
+      {/* <AppleCTAButton />
       <GoogleCTAButton />
-      <MantaCTAButton />
+      <MantaCTAButton /> */}
     </div>
   );
 }
