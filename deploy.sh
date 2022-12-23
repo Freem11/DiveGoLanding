41fbb44ce4f199e299877483192a9658989024ed
freem11 <freem1985@gmail.com>
2022-12-23 11:23:20 -0700

    another try

--- deploy.sh
+++ deploy.sh
@@ -21,9 +21,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
- git push -f git@github.com:freem11/freem11.github.io.git master
+# git push -f git@github.com:freem11/freem11.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+ git push -f git@github.com:freem11/DiveGoLanding.git main:gh-pages
 
 cd -
