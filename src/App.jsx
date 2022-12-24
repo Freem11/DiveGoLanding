e7c69e14b81100a6760f43bd019f373861a32d0e
freem11 <freem1985@gmail.com>
2022-12-24 11:35:44 -0700

    hero converted to youtube link to save app size

--- src/App.jsx
+++ src/App.jsx
@@ -11,29 +11,90 @@ import Timeline from "./components/timeline";
 import CallToActionsB from "./components/callToActionsB";
 import PromoPlayer from "./components/PromoPlayer";
 import WhyVidPlayer from "./components/WhyVidPlayer";
-import heroVid from "/videos/heroCompressed.mp4";
+// import heroVid from "https://youtu.be/ytdBFpVxYrA";
 import "./App.css";
+import ReactPlayer from "react-player/youtube";
 
-let heroWidth = window.width;
-let heroHeigth = 800;
+let screenInital = window.innerWidth;
+let heroWidth
+let heroHeigth
+if (screenInital < 501) {
+  heroHeigth = 600;
+  heroWidth = 600*(16/9);
+} else if (screenInital < 881) {
+  heroHeigth = 700;
+  heroWidth = 700*(16/9);
+} else if (screenInital < 1399) {
+  heroHeigth = 1000;
+  heroWidth = 1000*(16/9);
+} else if (screenInital < 1749) {
+  heroHeigth = 1000;
+  heroWidth = 1000*(16/9);
+}else {
+  heroHeigth = 1500;
+  heroWidth = 1500*(16/9);
+}
 
-console.log(heroWidth, heroHeigth);
+
+let Heighty
+let Widthy
+// console.log(heroWidth, heroHeigth);
 function App() {
+  const [vidWidth, setVidWidth] = useState(heroWidth);
+  const [vidHeigth, setVidHeigth] = useState(heroHeigth);
+
+  window.addEventListener('resize', trackWidth);
+
+  function trackWidth() {
+    if (window.innerWidth < 501) {
+      Heighty = 600;
+      Widthy = 600*(16/9);
+    } else if (window.innerWidth < 881) {
+      Heighty = 700;
+      Widthy = 700*(16/9);
+    } else if (window.innerWidth < 1399) {
+      Heighty = 1000;
+      Widthy = 1000*(16/9);
+    } else if (window.innerWidth < 1749) {
+      Heighty = 1000;
+      Widthy = 1000*(16/9);
+    }else {
+      Heighty = 1500;
+      Widthy = 1500*(16/9);
+    }
+    setVidHeigth(Heighty);
+    setVidWidth(Widthy);
+  }
+
+  console.log('H:', vidHeigth)
+  console.log('W:', vidWidth)
+
   return (
     <div className="wrapperdivMain">
       <div style={{ minHeight: "1000px", maxHeight: "auto" }}>
         <Logo />
 
         <div className="topsection">
+          <ReactPlayer
+            url="https://youtu.be/ytdBFpVxYrA"
+            width={vidWidth}
+            height={vidHeigth}
+            controls={false}
+            muted={true}
+            playing={true}
+            loop={true}
+            playsinline={true}
+            className="mainherovideo"
+          />
 
-            <video
-              src={heroVid}
-              autoPlay
-              loop
-              muted
-              playsInline
-              className="mainherovideo"
-            />
+          {/* <video
+            src={heroVid}
+            autoPlay
+            loop
+            muted
+            playsInline
+            className="mainherovideo"
+          /> */}
 
           <div className="leftTop">
             <HeadLine />
